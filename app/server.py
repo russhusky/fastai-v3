39a19eba6a552d7e7b2b9e7285d1d97722ac9c73
Russ Husky <russ.husky@cccom.com>
2019-04-09 04:38:55 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/file/d/1reEVPPAWKzsVq3jnF9yvThoVkWfwDn1G/view?usp=sharing'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1reEVPPAWKzsVq3jnF9yvThoVkWfwDn1G'
 export_file_name = 'export.pkl'
 
 classes = ['brass', 'wood', 'machine']
